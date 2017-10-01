3a8bd1048d5a5ffdfd4be2d7e1729045d8bacdae
RuobingL <rbing_lyu@gwmail.gwu.edu>
2017-09-30 20:00:07 -0400

    Initial commit

---
+++ app/src/main/java/edu/gwu/rbing_lyu/metro_explorer_hahaha/MenuActivity.kt
@@ -0,0 +1,12 @@
+package edu.gwu.rbing_lyu.metro_explorer_hahaha
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
