21c2961787545dcd742f1bd9f6ef91053b9fdb20
RuobingL <rbing_lyu@gwmail.gwu.edu>
2017-10-14 18:27:00 -0400

    Landmark.kt && update the project structure

---
+++ app/src/main/java/edu/gwu/rbing_lyu/metro_explorer_hahaha/model/Landmark.kt
@@ -0,0 +1,8 @@
+package edu.gwu.rbing_lyu.metro_explorer_hahaha.model
+
+import android.location.Location
+
+/**
+ * Created by ruobinglyu on 10/14/17.
+ */
+data class Landmark(val name: String, val location: Location, val image: String, val description: String)
