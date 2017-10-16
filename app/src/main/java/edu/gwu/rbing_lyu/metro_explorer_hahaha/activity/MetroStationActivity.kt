15872c355db82338fedf25539da4ab59bcf6fc25
faithgiguere <faithgiguere@gmail.com>
2017-10-15 20:16:04 -0400

    Implemented the MetroStationsAdapter to properly load the metro stations into a RecyclerView in the MetroStationActivity, which are tied to onClick events that currently just log the name of the station clicked. Modified styles a bit as well. Non-functional search bar is now at the top of the MetroStationActivity as well.

--- app/src/main/java/edu/gwu/rbing_lyu/metro_explorer_hahaha/activity/MetroStationActivity.kt
+++ app/src/main/java/edu/gwu/rbing_lyu/metro_explorer_hahaha/activity/MetroStationActivity.kt
@@ -2,32 +2,47 @@ package edu.gwu.rbing_lyu.metro_explorer_hahaha.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import edu.gwu.rbing_lyu.metro_explorer_hahaha.R
+import edu.gwu.rbing_lyu.metro_explorer_hahaha.adapter.MetroStationsAdapter
 import edu.gwu.rbing_lyu.metro_explorer_hahaha.model.MetroStation
 import edu.gwu.rbing_lyu.metro_explorer_hahaha.utils.FetchMetroStationsManager
+import kotlinx.android.synthetic.main.activity_select_station.*
 
 
-class MetroStationActivity : AppCompatActivity(), FetchMetroStationsManager.FetchMetroStationsListener{
+class MetroStationActivity : AppCompatActivity(), FetchMetroStationsManager.FetchMetroStationsListener, MetroStationsAdapter.OnItemClickedListener {
     private val TAG = "MetroStationActivity"
 
     // Helper declarations
     private lateinit var fetchMetroStationsManager: FetchMetroStationsManager
+    private lateinit var metroStationsAdapter: MetroStationsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_station)
 
+        // Initialize and fetch list of metro stations fro WMATA API
         fetchMetroStationsManager = FetchMetroStationsManager(this)
         fetchMetroStationsManager.fetchMetroStationsListener = this
 
         fetchMetroStationsManager.listStations()
     }
 
+    override fun onItemClick(position: Int) {
+        // For now, just a debug log of the MetroStation's name clicked
+        Log.d(TAG, metroStationsAdapter.getItem(position).name)
+    }
+
     override fun stationsLoaded(stations: List<MetroStation>) {
-        // Debug log for now, verify correctly receives list of MetroStation objects parsed
-        // from WMATA API
-        Log.d(TAG, stations.toString())
+        // Use adapter and recycler view handler to fill list of stations
+        metroStationsAdapter = MetroStationsAdapter(stations)
+
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.adapter = metroStationsAdapter
+
+        // Set the onClick listener for the adapter
+        metroStationsAdapter.onItemClickedListener = this
     }
 
     override fun stationsNotLoaded() {
--- app/src/main/java/edu/gwu/rbing_lyu/metro_explorer_hahaha/adapter/MetroStationsAdapter.kt
+++ app/src/main/java/edu/gwu/rbing_lyu/metro_explorer_hahaha/adapter/MetroStationsAdapter.kt
@@ -1,5 +1,70 @@
 package edu.gwu.rbing_lyu.metro_explorer_hahaha.adapter
 
+import android.content.Context
+import android.content.Intent
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.TextView
+import edu.gwu.rbing_lyu.metro_explorer_hahaha.R
+import edu.gwu.rbing_lyu.metro_explorer_hahaha.activity.MetroStationActivity
+import edu.gwu.rbing_lyu.metro_explorer_hahaha.model.MetroStation
+import edu.gwu.rbing_lyu.metro_explorer_hahaha.activity.LandmarkDetailActivity
+import edu.gwu.rbing_lyu.metro_explorer_hahaha.activity.MenuActivity
 
-class MetroStationsAdapter {
+
+class MetroStationsAdapter(private val stations: List<MetroStation>) : RecyclerView.Adapter<MetroStationsAdapter.ViewHolder>() {
+    private val TAG = "MetroStationsAdapter"
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        // Obtain station at position
+        val station = stations?.get(position)
+
+        // Bind station to view holder
+        station?.let {
+            (holder as ViewHolder).bind(station)
+        }
+    }
+
+    // Function to retrieve a station by ID
+    fun getItem(position: Int): MetroStation {
+        return stations[position]
+    }
+
+    // Set up interface to be able to set OnClick events for items (stations)
+    var onItemClickedListener: OnItemClickedListener? = null
+
+    interface OnItemClickedListener {
+        fun onItemClick(position: Int)
+    }
+
+    override fun getItemCount(): Int {
+        return stations.count()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent?.context)
+
+        // Inflate our station row layout
+        return ViewHolder(layoutInflater.inflate(R.layout.row_metro_station, parent, false))
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+        private val stationNameView: TextView = view.findViewById(R.id.stationName)
+
+        // Update metro station row UI with station name
+        fun bind(station: MetroStation) {
+            stationNameView.text = station.name.replace("\"", "")
+            // And set the onClickListener
+            stationNameView.setOnClickListener(this)
+        }
+
+        // Specify onClick to be passed to the interfaced function
+        override fun onClick(p0: View?) {
+            onItemClickedListener?.onItemClick(adapterPosition)
+        }
+    }
 }
